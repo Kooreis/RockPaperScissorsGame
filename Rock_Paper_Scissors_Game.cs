981174a41639222300764f9fa903427adad77b18
Kooreis <kooreis.official@gmail.com>
2022-11-28 17:00:00 -0600

    Initial setup of the RockPaperScissors game. Declared the choices available, initialized the random number generator, and set up variables for the computer's choice, the user's choice, and both scores.

--- Rock_Paper_Scissors_Game.cs
+++ Rock_Paper_Scissors_Game.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 namespace RockPaperScissors
@@ -11,40 +10,6 @@ namespace RockPaperScissors
             Random rand = new Random();
             string computerChoice, userChoice;
             int computerScore = 0, userScore = 0;
-
-            while (true)
-            {
-                Console.WriteLine("Enter Rock, Paper or Scissors (or Quit to exit): ");
-                userChoice = Console.ReadLine();
-
-                if (userChoice.ToLower() == "quit")
-                {
-                    break;
-                }
-
-                computerChoice = choices[rand.Next(choices.Length)];
-                Console.WriteLine("Computer chose " + computerChoice);
-
-                if (userChoice == computerChoice)
-                {
-                    Console.WriteLine("It's a draw!");
-                }
-                else if ((userChoice == "Rock" && computerChoice == "Scissors") ||
-                         (userChoice == "Paper" && computerChoice == "Rock") ||
-                         (userChoice == "Scissors" && computerChoice == "Paper"))
-                {
-                    Console.WriteLine("You win!");
-                    userScore++;
-                }
-                else
-                {
-                    Console.WriteLine("Computer wins!");
-                    computerScore++;
-                }
-
-                Console.WriteLine("Score: You - " + userScore + " Computer - " + computerScore);
-            }
         }
     }
 }
-```
