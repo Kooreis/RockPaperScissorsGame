70ddb4f6aacac3f5afe523cd84b0e25ade92ef28
Kooreis <kooreis.official@gmail.com>
2022-11-29 18:00:00 -0600

    Created the main class and method. Initialized the scanner for user input, the array for the game choices, and the random class for computer choice. The computer's choice is randomly selected and the user is prompted to enter their choice.

--- Rock_Paper_Scissors_Game.java
+++ Rock_Paper_Scissors_Game.java
@@ -1,2 +1,10 @@
-import java.util.Random;
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] rps = {"Rock", "Paper", "Scissors"};
+        Random random = new Random();
+        int computerInt = random.nextInt(rps.length);
+        String computerChoice = rps[computerInt];
+        System.out.println("Enter Rock, Paper or Scissors:");
+        String userChoice = scanner.nextLine();
+        System.out.println("Computer chose: " + computerChoice);
